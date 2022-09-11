2108336a655ed5a7cdfd8c714c0e66f2e140384a
Piedad García Ortega <piedad.pgo@gmail.com>
2022-09-11 16:51:09 +0200

    Program to sum two numbers

---
+++ Suma/src/suma/Suma.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma;
+
+/**
+ *
+ * @author Piedad
+ */
+public class Suma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero_uno = 5;
+        int numero_dos = 2;
+        int resultado = 0; 
+        
+        resultado = numero_uno + numero_dos;
+        
+        System.out.println ("El resultado de la suma es: "+resultado);
+    }
+    
+}
